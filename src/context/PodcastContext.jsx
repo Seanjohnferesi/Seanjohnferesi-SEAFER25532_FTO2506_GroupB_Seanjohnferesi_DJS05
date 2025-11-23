a8417aa2cfce7d8fec1195a1bd218e47b6e2f8e2
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-11-23 14:23:35 +0200

    refactor: move filtering, sorting, and search logic into PodcastContext

--- src/context/PodcastContext.jsx
+++ src/context/PodcastContext.jsx
@@ -1,24 +1,36 @@
 import { createContext, useContext, useState } from "react";
+import { filterPodcasts } from "../utils/filterPodcasts.js";
+import { sortPodcasts } from "../utils/sortPodcast.js";
+import { searchPodcast } from "../utils/search.js";
+import { genres } from "../data.js";
+import { getGenreTitle } from "../utils/getGenreTitle.js";
+
 
 const PodcastContext = createContext();
 
 export const usePodcast = () => useContext(PodcastContext)
 
-export function Podcast({children}) {
+export function Podcast({ children }) {
     const [podcasts, setPodcasts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [selectedPodcast, setSelectedPodcast] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerpage = 6;
-    const totalItems = podcasts.length;
     const [sort, setSort] = useState("");
     const [selectedGenre , setSelectedGenre] = useState("");
     const [searchInput, setSearchInput] = useState("");
-    const [seasons, setSeasons] = useState([])
+    const [seasons, setSeasons] = useState([]);
+    const [selectedSeason, setSelectedSeason] = useState(0);
+
+    // These need to be inside the component
+    const filtered = filterPodcasts(podcasts, selectedGenre, genres, getGenreTitle);
+    const sortedItems = sortPodcasts(filtered, sort);
+    const searchFiltered = searchPodcast(sortedItems, searchInput);
+    const totalItems = searchFiltered.length;
 
     return (
-        <PodcastContext.Provider value = {{
+        <PodcastContext.Provider value={{
             podcasts,
             setPodcasts,
             loading,
@@ -30,6 +42,7 @@ export function Podcast({children}) {
             currentPage,
             setCurrentPage,
             itemsPerpage,
+            searchFiltered,
             totalItems,
             sort,
             setSort,
@@ -38,7 +51,9 @@ export function Podcast({children}) {
             searchInput,
             setSearchInput,
             seasons,
-            setSeasons
+            setSeasons,
+            selectedSeason,
+            setSelectedSeason
         }}>
             {children}
         </PodcastContext.Provider>
